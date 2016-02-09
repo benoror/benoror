287a6243c80a3f4f282c6eacb0fe7241fbb1e5e1
Benjamin Orozco <benoror@gmail.com>
2016-02-09 10:48:58 -0600

    fix(config/environment.js): ENV.baseURL

--- config/environment.js
+++ config/environment.js
@@ -40,7 +40,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.baseURL = '/benoror.github.com';
+    ENV.baseURL = '/benoror.github.io';
   }
 
   return ENV;

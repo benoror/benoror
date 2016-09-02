cb257dbb512c375923a51bae0e3be375891dd289
Ben Orozco <benoror@gmail.com>
2016-09-02 18:37:41 -0500

    Release

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('pgp');
   this.route('cv');
+  this.route('resume');
 });
 
 export default Router;
---
+++ app/routes/resume.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+  ajax: Ember.inject.service(),
+
+  model() {
+    return this.get('ajax').request('/assets/resume.json');
+  }
+});

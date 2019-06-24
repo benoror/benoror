f093cbefd26d3365535939faf5af314713eff065
Ben Orozco <benoror@gmail.com>
2019-06-23 20:12:22 -0600

    Chore: Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -220,5 +220,11 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-76368021-1",
+      },
+    },
   ],
 }

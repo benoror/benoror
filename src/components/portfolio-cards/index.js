f1ae18df2498145e49589d41af5c9c30832d21a8
Ben Orozco <benoror@gmail.com>
2019-06-23 19:38:44 -0600

    Feature[Portfolio]: Font Awesome icons

--- src/components/portfolio-cards/index.js
+++ src/components/portfolio-cards/index.js
@@ -2,6 +2,10 @@ import React from "react"
 import Card from "card-vibes"
 import styles from "./styles.module.css"
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faLink } from "@fortawesome/free-solid-svg-icons"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+
 const PortfolioCards = ({ cards }) => {
   return (
     <div className={styles.container}>
@@ -19,16 +23,23 @@ const PortfolioCards = ({ cards }) => {
                   src={require(`./images/${title}.png`)}
                 />
               </a>
-              <h4>
-                <a href={url}>{title}</a>
-              </h4>
-              <p> </p>
+              <h4> {title} </h4>
             </div>
             <div className={`${styles.back} ${styles.side}`}>
               <h4>
                 <a href={url}>{title}</a>
               </h4>
               <p>{description}</p>
+              <div className={styles.links}>
+                <a href={url}>
+                  <FontAwesomeIcon icon={faLink} />
+                </a>
+                {links && links.github && (
+                  <a href={links.github}>
+                    <FontAwesomeIcon icon={faGithub} />
+                  </a>
+                )}
+              </div>
             </div>
           </div>
         </Card>

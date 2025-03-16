d51863d9d8328b4e963bfe8f0384a7edea4150f5
Ben Orozco <benoror@gmail.com>
2025-03-15 22:42:23 -0600

    add utils package

--- packages/data/src/personal.ts
+++ packages/data/src/personal.ts
@@ -1,4 +1,5 @@
 export const PERSONAL = {
   full_name: "Benjamin Orozco Rios",
   short_name: 'Ben Orozco',
+  private_email: 'benoror@gmail.com',
 }
--- packages/data/src/resume.ts
+++ packages/data/src/resume.ts
@@ -1,6 +1,8 @@
-import { PERSONAL } from './personal';
+import { PERSONAL } from '@workspace/data/personal';
+import { gmailAlias } from '@workspace/utils/email';
 
 export const RESUME = {
   name: PERSONAL.short_name,
   header: 'Tech Lead - Software Engineer - Full Stack Developer',
+  public_email: gmailAlias(PERSONAL.private_email, 'cv')
 }

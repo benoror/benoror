f8dc1bf99b906565d7f92dc375ca94c0d78676a5
Ben Orozco <benoror@gmail.com>
2025-04-15 15:54:32 -0600

    Set default themes for Web & Resume

--- apps/resume/components/providers.tsx
+++ apps/resume/components/providers.tsx
@@ -7,7 +7,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="light"
       enableSystem
       disableTransitionOnChange
       enableColorScheme
--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -7,7 +7,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="dark"
       enableSystem
       disableTransitionOnChange
       enableColorScheme
